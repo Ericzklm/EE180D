b3db597b62d44e0b837a5a6ae1b7928fd6f0e6e9
Eric Zhang <ericzklm@hotmail.com>
2021-10-16 01:28:35 +0100

    Created publisher for mqtt broker

---
+++ clientTest.py
@@ -0,0 +1,21 @@
+import socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('192.168.1.15', 8080))
+client.send('I am CLIENT'.encode())
+from_server = client.recv(4096)
+client.close()
+print(from_server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
